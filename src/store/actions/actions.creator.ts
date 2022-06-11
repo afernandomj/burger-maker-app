274ec529679e4dd4375eb3a141482d09ff7db8fa
Andres Fernando Moreno <andres.moreno@pluto.tv>
2022-06-11 12:28:10 -0500

    [add] actions creator

---
+++ src/store/actions/actions.creator.ts
@@ -0,0 +1,38 @@
+import { createAction } from '@reduxjs/toolkit';
+import { BURGER_ACTION_BASE } from '../Burger.constants';
+import { IngredientActionPayload, IngredientsItem } from '../Burger.types';
+
+export const burgerActions = {
+	addItem: {
+		init: createAction<IngredientActionPayload>(
+			`${BURGER_ACTION_BASE}/add-ingredient/init`
+		),
+		success: createAction<IngredientsItem>(
+			`${BURGER_ACTION_BASE}/add-ingredient/success`
+		),
+		set: createAction<IngredientsItem>(
+			`${BURGER_ACTION_BASE}/add-ingredient/set`
+		),
+	},
+	removeItem: {
+		init: createAction<IngredientActionPayload>(
+			`${BURGER_ACTION_BASE}/remove-ingredient/init`
+		),
+		success: createAction<IngredientActionPayload>(
+			`${BURGER_ACTION_BASE}/remove-ingredient/success`
+		),
+		set: createAction<IngredientActionPayload>(
+			`${BURGER_ACTION_BASE}/remove-ingredient/set`
+		),
+	},
+	getItems: {
+		init: createAction(`${BURGER_ACTION_BASE}/get-ingredients/init`),
+		success: createAction<IngredientsItem[]>(
+			`${BURGER_ACTION_BASE}/get-ingredients/success`
+		),
+		set: createAction<IngredientsItem[]>(
+			`${BURGER_ACTION_BASE}/get-ingredients/set`
+		),
+	},
+};
+
