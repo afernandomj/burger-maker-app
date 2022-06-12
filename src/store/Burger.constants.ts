db60baf1dd5f1a92fb7774530692364fb1e1b38b
Andres Fernando Moreno <andres.moreno@pluto.tv>
2022-06-12 13:57:45 -0500

    [add] environment variables to the constant file

--- src/store/Burger.constants.ts
+++ src/store/Burger.constants.ts
@@ -1,2 +1,3 @@
 export const BURGER_ACTION_BASE = '@@burger';
-
+export const BURGER_API_BASE = `${process.env.REACT_APP_API_URL}`;
+export const BURGER_INGREDIENTS_ENDPOINT = `${process.env.REACT_APP_INGREDIENTS_ENDPOINT}`;
