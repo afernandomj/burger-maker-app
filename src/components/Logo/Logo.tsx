65ccfcaa26100abbc7c88c5d6466c6a802c842b1
Andres Fernando Moreno <andres.moreno@pluto.tv>
2022-06-12 21:36:07 -0500

    [add] logo component

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import logoStyles from './Logo.module.css';
+
+import burgerLogo from '../../assets/images/burger-logo.png';
 
 const Logo = () => {
-	return <div>Logo</div>;
+	return (
+		<div className={logoStyles.LogoWrapper}>
+			<div className={logoStyles.Logo}>
+				<div
+					className={logoStyles.LogoInner}
+					style={{ backgroundImage: `url(${burgerLogo})` }}
+				></div>
+			</div>
+		</div>
+	);
 };
 
 export default Logo;
