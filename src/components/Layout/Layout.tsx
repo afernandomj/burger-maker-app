13d93bf4bbbfc47308ea4c14b82b2ef667f0d34f
Andres Fernando Moreno <andres.moreno@pluto.tv>
2022-06-14 22:55:34 -0500

    passed selector for ingredients into BuildControls through by Layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,6 @@
 import { PropsWithChildren } from 'react';
+import { useAppSelector } from '../../hooks';
+import { selectBurgerIngredients } from '../../store/reducer/burger.duck';
 import BuildControls from '../BuildControls/BuildControls';
 import Toolbar from '../Toolbar/Toolbar';
 
@@ -7,11 +9,12 @@ import layoutStyles from './Layout.module.css';
 type LayoutProps = PropsWithChildren<{}>;
 
 const Layout = ({ children }: LayoutProps) => {
+	const burgerIngredients = useAppSelector(selectBurgerIngredients);
 	return (
 		<div className={layoutStyles.Layout}>
 			<Toolbar />
 			<main className={layoutStyles.MainWrapper}>{children}</main>
-			<BuildControls />
+			<BuildControls ingredients={burgerIngredients} />
 		</div>
 	);
 };
