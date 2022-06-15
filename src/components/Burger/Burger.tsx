8ed79edd1e00e0fb7448ea5eacf77fc27b3848d3
Andres Fernando Moreno <andres.moreno@pluto.tv>
2022-06-14 22:05:57 -0500

    [add] Burger component

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import burgerStyles from './Burger.module.css';
+import BurgerIngredient from '../BurgerIngredient/BurgerIngredient';
+import { IngredientsItem } from '../../store/Burger.types';
+import { useAppSelector } from '../../hooks';
+import {
+	selectBurgerError,
+	selectBurgerLoading,
+} from '../../store/reducer/burger.duck';
 
-const Burger = () => {
-	return <div>Burger</div>;
+type TBurgerProps = {
+	ingredients: IngredientsItem[];
+};
+
+const Burger = ({ ingredients }: TBurgerProps) => {
+	const isLoading = useAppSelector(selectBurgerLoading);
+	const error = useAppSelector(selectBurgerError);
+	const updatedIngredients = ingredients.map((ingredient) => (
+		<BurgerIngredient key={ingredient._id} type={ingredient.type} />
+	));
+	const isIngredientsEmpty = ingredients.length === 0;
+	const loadingText = isLoading ? 'Loading...' : '';
+	let errorMessage = null;
+	if (!isLoading && isIngredientsEmpty) {
+		errorMessage = (
+			<>
+				<p>{error}</p>
+				<p>Get started to add some ingredients to build your burger!</p>
+			</>
+		);
+	}
+
+	return (
+		<div className={burgerStyles.Burger}>
+			<BurgerIngredient type='bread-top' />
+			{loadingText}
+			{errorMessage}
+			{updatedIngredients}
+			<BurgerIngredient type='bread-bottom' />
+		</div>
+	);
 };
 
 export default Burger;
