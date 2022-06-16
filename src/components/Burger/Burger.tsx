5290d0cf85b89d46855a4e9b073d36198fe23346
Andres Fernando Moreno <andres.moreno@pluto.tv>
2022-06-15 23:19:47 -0500

    added counter validation to the Burger

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -14,9 +14,11 @@ type TBurgerProps = {
 const Burger = ({ ingredients }: TBurgerProps) => {
 	const isLoading = useAppSelector(selectBurgerLoading);
 	const error = useAppSelector(selectBurgerError);
-	const updatedIngredients = ingredients.map((ingredient) => (
-		<BurgerIngredient key={ingredient._id} type={ingredient.type} />
-	));
+	const updatedIngredients = ingredients.map((ingredient) =>
+		ingredient.count > 0 ? (
+			<BurgerIngredient key={ingredient._id} type={ingredient.type} />
+		) : null
+	);
 	const isIngredientsEmpty = ingredients.length === 0;
 	const loadingText = isLoading ? 'Loading...' : '';
 	let errorMessage = null;
