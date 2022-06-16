e98d3c8c67034bd439fc3bc123e3641bd85e794d
Andres Fernando Moreno <andres.moreno@pluto.tv>
2022-06-16 00:18:27 -0500

    updated Burger to show ingredients filtered

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -14,12 +14,14 @@ type TBurgerProps = {
 const Burger = ({ ingredients }: TBurgerProps) => {
 	const isLoading = useAppSelector(selectBurgerLoading);
 	const error = useAppSelector(selectBurgerError);
-	const updatedIngredients = ingredients.map((ingredient) =>
-		ingredient.count > 0 ? (
-			<BurgerIngredient key={ingredient._id} type={ingredient.type} />
-		) : null
-	);
-	const isIngredientsEmpty = ingredients.length === 0;
+	let updatedIngredients = ingredients
+		.map((ingredient) =>
+			ingredient.count > 0 ? (
+				<BurgerIngredient key={ingredient._id} type={ingredient.type} />
+			) : null
+		)
+		.filter((ingredient) => ingredient !== null);
+	const isIngredientsEmpty = updatedIngredients.length === 0;
 	const loadingText = isLoading ? 'Loading...' : '';
 	let errorMessage = null;
 	if (!isLoading && isIngredientsEmpty) {
