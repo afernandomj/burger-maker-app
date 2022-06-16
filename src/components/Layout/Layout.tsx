5ef34c80d2856d1cfa95d4d9f21eb0281d8be84b
Andres Fernando Moreno <andres.moreno@pluto.tv>
2022-06-15 23:21:41 -0500

    removed BuildControls from Layout, and position this one relatively

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,4 @@
 import { PropsWithChildren } from 'react';
-import { useAppSelector } from '../../hooks';
-import { selectBurgerIngredients } from '../../store/reducer/burger.duck';
-import BuildControls from '../BuildControls/BuildControls';
 import Toolbar from '../Toolbar/Toolbar';
 
 import layoutStyles from './Layout.module.css';
@@ -9,12 +6,10 @@ import layoutStyles from './Layout.module.css';
 type LayoutProps = PropsWithChildren<{}>;
 
 const Layout = ({ children }: LayoutProps) => {
-	const burgerIngredients = useAppSelector(selectBurgerIngredients);
 	return (
 		<div className={layoutStyles.Layout}>
 			<Toolbar />
 			<main className={layoutStyles.MainWrapper}>{children}</main>
-			<BuildControls ingredients={burgerIngredients} />
 		</div>
 	);
 };
