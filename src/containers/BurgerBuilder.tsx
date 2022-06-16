ab7e1efde249f65bac0cae20d4966a14f4f9197f
Andres Fernando Moreno <andres.moreno@pluto.tv>
2022-06-16 00:18:49 -0500

    dispatch add and remove action from BurgerBuilder

--- src/containers/BurgerBuilder.tsx
+++ src/containers/BurgerBuilder.tsx
@@ -5,11 +5,16 @@ import BuildControls from '../components/BuildControls/BuildControls';
 import Burger from '../components/Burger/Burger';
 import { useAppSelector } from '../hooks';
 import { burgerActions } from '../store/actions/actions.creator';
+import { IngredientActionPayload } from '../store/Burger.types';
 import { selectBurgerIngredients } from '../store/reducer/burger.duck';
 
 type TBurgerBuilderProps = ReturnType<typeof mapDispatchToProps>;
 
-const BurgerBuilder = ({ getIngredients }: TBurgerBuilderProps) => {
+const BurgerBuilder = ({
+	getIngredients,
+	addIngredient,
+	removeIngredient,
+}: TBurgerBuilderProps) => {
 	useEffect(() => {
 		getIngredients();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
@@ -22,18 +27,18 @@ const BurgerBuilder = ({ getIngredients }: TBurgerBuilderProps) => {
 			<Burger ingredients={burgerIngredients} />
 			<BuildControls
 				ingredients={burgerIngredients}
-				onAddedIngredient={() => {}}
-				onRemovedIngredient={() => {}}
+				onAddedIngredient={(id: number) => addIngredient({ _id: id })}
+				onRemovedIngredient={(id: number) => removeIngredient({ _id: id })}
 			/>
 		</>
 	);
 };
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-	// 	addIngredient: (payload: IngredientActionPayload) =>
-	// 		dispatch(burgerActions.addItem.init(payload)),
-	// 	removeIngredient: (payload: IngredientActionPayload) =>
-	// 		dispatch(burgerActions.removeItem.init(payload)),
+	addIngredient: (payload: IngredientActionPayload) =>
+		dispatch(burgerActions.addItem.init(payload)),
+	removeIngredient: (payload: IngredientActionPayload) =>
+		dispatch(burgerActions.removeItem.init(payload)),
 	getIngredients: () => dispatch(burgerActions.getItems.init()),
 });
 
