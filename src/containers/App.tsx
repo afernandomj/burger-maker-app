49924943ba25905b761dbcc9b8695418135ac836
Andres Fernando Moreno <andres.moreno@pluto.tv>
2022-06-16 00:19:19 -0500

    linked everything to the App component

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,53 +1,42 @@
-import { Dispatch } from '@reduxjs/toolkit';
-import { useEffect } from 'react';
-import { connect } from 'react-redux';
-import { useAppSelector } from '../hooks';
-import { burgerActions } from '../store/actions/actions.creator';
-import {
-	IngredientActionPayload,
-	IngredientsItem,
-} from '../store/Burger.types';
-import { selectBurgerIngredients } from '../store/reducer/burger.duck';
+import Layout from '../components/Layout/Layout';
+import BurgerBuilder from './BurgerBuilder';
 
-const App = (props: any) => {
-	const burgerIngredients = useAppSelector(selectBurgerIngredients);
-
-	useEffect(() => {
-		props.getIngredients();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-
+const App = () => {
 	return (
-		<div>
-			<h1>Burger Marker App!</h1>
-			<ul>
-				{burgerIngredients &&
-					burgerIngredients.map((ingredient: IngredientsItem) => (
-						<li key={ingredient._id}>
-							{ingredient.label} ({ingredient.price}) -{' '}
-							{ingredient.type}{' '}
-							<button
-								onClick={() =>
-									props.removeIngredient({
-										_id: ingredient._id,
-									})
-								}
-							>
-								Remove
-							</button>
-						</li>
-					))}
-			</ul>
-		</div>
+		<Layout>
+			<BurgerBuilder />
+		</Layout>
+		// <div>
+		// 	<h1>Burger Marker App!</h1>
+		// 	<ul>
+		// 		{burgerIngredients &&
+		// 			burgerIngredients.map((ingredient: IngredientsItem) => (
+		// 				<li key={ingredient._id}>
+		// 					{ingredient.label} ({ingredient.price}) -{' '}
+		// 					{ingredient.type}{' '}
+		// 					<button
+		// 						onClick={() =>
+		// 							props.removeIngredient({
+		// 								_id: ingredient._id,
+		// 							})
+		// 						}
+		// 					>
+		// 						Remove
+		// 					</button>
+		// 				</li>
+		// 			))}
+		// 	</ul>
+		// </div>
 	);
 };
 
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-	addIngredient: (payload: IngredientActionPayload) =>
-		dispatch(burgerActions.addItem.init(payload)),
-	removeIngredient: (payload: IngredientActionPayload) =>
-		dispatch(burgerActions.removeItem.init(payload)),
-	getIngredients: () => dispatch(burgerActions.getItems.init()),
-});
+// const mapDispatchToProps = (dispatch: Dispatch) => ({
+// 	addIngredient: (payload: IngredientActionPayload) =>
+// 		dispatch(burgerActions.addItem.init(payload)),
+// 	removeIngredient: (payload: IngredientActionPayload) =>
+// 		dispatch(burgerActions.removeItem.init(payload)),
+// 	getIngredients: () => dispatch(burgerActions.getItems.init()),
+// });
 
-export default connect(null, mapDispatchToProps)(App);
+// export default connect(null, mapDispatchToProps)(App);
+export default App;
