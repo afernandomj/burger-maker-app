9fd00d6ff79989f8feb47ec1cd33e97ad8df201b
Andres Fernando Moreno <98559762+afernandomj@users.noreply.github.com>
2023-12-11 23:00:48 -0500

    [add] own npm packages to log messages through console and make http requests (#2)
    Co-authored-by: Ferdroid8080 <andresf@ferudinato.com>

--- src/store/sagas/addItem.ts
+++ src/store/sagas/addItem.ts
@@ -3,10 +3,13 @@ import { SagaIterator } from 'redux-saga';
 import { IngredientActionPayload } from '../../types/store';
 import { put } from 'redux-saga/effects';
 import { addItemSet } from '../actions/Creator';
+import clilogger from 'bmcli-logger';
 
 export function* addedItem(
 	action: PayloadAction<IngredientActionPayload>
 ): SagaIterator<void> {
 	// console.log('[sagas/index.ts] [addedItem]');
+	clilogger.info('[sagas/index.ts] [addedItem]');
+
 	yield put(addItemSet({ _id: action.payload._id }));
 }
--- src/store/sagas/removeItem.ts
+++ src/store/sagas/removeItem.ts
@@ -3,11 +3,13 @@ import { SagaIterator } from 'redux-saga';
 import { IngredientActionPayload } from '../../types/store';
 import { put } from 'redux-saga/effects';
 import { removeItemSet } from '../actions/Creator';
+import clilogger from 'bmcli-logger';
 
 export function* removedItem(
 	action: PayloadAction<IngredientActionPayload>
 ): SagaIterator<void> {
 	// console.log('[sagas/index.ts] [removedItem]');
+	clilogger.info('[sagas/index.ts] [removedItem]');
 	yield put({
 		type: removeItemSet.type,
 		payload: action.payload,
